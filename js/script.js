4aa77d6da1112ab0677fbe86588f801a93b335c9
ssooonnnn <soonhy1020@gamil.com>
2023-06-07 12:42:51 +0900

    header : scroll opacity

--- js/script.js
+++ js/script.js
@@ -1,15 +1,25 @@
 $(document).ready(function(){
-            // cursor 
-            $(function() {
-                var prefix = function() {
-                  var a = window.getComputedStyle(document.documentElement, ""),
-                    b = (Array.prototype.slice.call(a).join("").match(/-(moz|webkit|ms)-/) || "" === a.OLink && ["", "o"])[1];
-                  return "WebKit|Moz|MS|O".match(new RegExp("(" + b + ")", "i"))[1], "-" + b + "-"
-                }();
-                $(document).mousemove(function(e) {
-                  mouseX = e.pageX + 15;
-                  mouseY = e.pageY - $(window).scrollTop() + 15;
-                  $('.theBall-outer').attr('style', prefix + 'transform:translate(' + mouseX + 'px,' + mouseY + 'px)');
-                });
-            })
+  // cursor 
+  $(function() {
+      var prefix = function() {
+        var a = window.getComputedStyle(document.documentElement, ""),
+          b = (Array.prototype.slice.call(a).join("").match(/-(moz|webkit|ms)-/) || "" === a.OLink && ["", "o"])[1];
+        return "WebKit|Moz|MS|O".match(new RegExp("(" + b + ")", "i"))[1], "-" + b + "-"
+      }();
+      $(document).mousemove(function(e) {
+        mouseX = e.pageX + 15;
+        mouseY = e.pageY - $(window).scrollTop() + 15;
+        $('.theBall-outer').attr('style', prefix + 'transform:translate(' + mouseX + 'px,' + mouseY + 'px)');
+      });
+  })
+
+  $(window).scroll(function(){ 
+    var height = $(document).scrollTop(); //실시간으로 스크롤의 높이를 측정
+    // console.log(height)
+    if(height >= 800){ 
+        $('header').addClass('on'); 
+    }else if(height <= 799){ 
+        $('header').removeClass('on'); 
+    } 
+  });
 });
